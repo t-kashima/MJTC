0a167cb52f9e9ece1b50d51cda3b2b50f0617aff
Kashima Takumi <kodokurider@gmail.com>
2013-10-12 14:32:46 +0900

    Initial Commit

---
+++ mujoticon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mujoticon
+//
+//  Created by Kashima Takumi on 2013/10/12.
+//  Copyright (c) 2013年 UNUUU FOUNDATION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mujoticon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mujoticon
+//
+//  Created by Kashima Takumi on 2013/10/12.
+//  Copyright (c) 2013年 UNUUU FOUNDATION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
